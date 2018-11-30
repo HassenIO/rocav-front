de2db1e57c838fef049d7a02addea4955e36fb73
Hassen Taidirt <htaidirt@gmail.com>
2018-11-30 19:39:03 +0100

    Fetch inputs from AWS Lambda

--- src/components/FilterInput.js
+++ src/components/FilterInput.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
 
 class FilterInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      values: props.values,
+    };
+  }
+
+  createOptions(values) {
+    if (!values) return '';
+
+    let items = [];
+    for (let value of values) {
+      items.push(
+        <option key={value} value={value}>
+          {value}
+        </option>,
+      );
+    }
+    return items;
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({ values: nextProps.values });
+  }
+
   render() {
     return (
       <div className="FilterInput">
-        <select>
-          <option value="volvo">Volvo</option>
-          <option value="saab">Saab</option>
-          <option value="mercedes">Mercedes</option>
-          <option value="audi">Audi</option>
-        </select>
+        <select>{this.createOptions(this.state.values)}</select>
       </div>
     );
   }
--- src/components/Filters.js
+++ src/components/Filters.js
@@ -3,12 +3,37 @@ import FilterInput from './FilterInput';
 import FilterButton from './FilterButton';
 
 class Filters extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputs: {},
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://v86y9ouqxb.execute-api.eu-west-1.amazonaws.com/dev/inputs')
+      .then(res => res.json())
+      .then(
+        result => {
+          this.setState({
+            inputs: result
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        error => {
+          console.log(error);
+        },
+      );
+  }
+
   render() {
     return (
       <div className="Filters">
-        <FilterInput />
-        <FilterInput />
-        <FilterInput />
+        <FilterInput values={this.state.inputs.inputsFilter ? this.state.inputs.inputsFilter.compTerit : []} />
+        <FilterInput values={this.state.inputs.inputsFilter ? this.state.inputs.inputsFilter.specialite : []} />
+        <FilterInput values={this.state.inputs.inputsFilter ? this.state.inputs.inputsFilter.domaine : []} />
         <FilterButton />
       </div>
     );
