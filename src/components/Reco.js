34fa3a9e3c789689d78c3fb71efc2bd29de81003
Hassen Taidirt <htaidirt@gmail.com>
2018-12-04 11:43:10 +0100

    Make texts in french

--- src/components/Reco.js
+++ src/components/Reco.js
@@ -10,11 +10,13 @@ class Reco extends Component {
       filters: {},
       reco: [],
       recoTable: [],
+      loading: false
     };
   }
 
   fetchReco(compTerit, specialite, domaine) {
     let self = this;
+    this.setState({ loading: true });
     fetch(
       `https://v86y9ouqxb.execute-api.eu-west-1.amazonaws.com/dev/reco?compTerit=${encodeURIComponent(
         compTerit,
@@ -27,6 +29,7 @@ class Reco extends Component {
         self.setState({
           reco: res,
           recoTable: this.buildRecoTable(res),
+          loading: false,
         });
       });
   }
@@ -35,10 +38,19 @@ class Reco extends Component {
     return reco.map(r => {
       const nbMissions = r[`nb_missions_${this.state.filters.domaine}`];
       return {
-        AvocatId: r.nameAvocat ? (<span className="avocat-name">{r.nameAvocat}</span>) : (<em className="avocat-id">{r.AvocatId}</em>),
+        AvocatId: r.nameAvocat ? (
+          <span className="avocat-name">{r.nameAvocat}</span>
+        ) : (
+          <em className="avocat-id">{r.AvocatId}</em>
+        ),
         Critères: `${Math.floor(r.Critères * 100)} %`,
         nb_missions: nbMissions === 0 ? '-' : nbMissions,
-        partenariat: r.Partenaire === 'Y' ? 'Partenaire' : (<span className="not-partner">Non partenaire</span>),
+        partenariat:
+          r.Partenaire === 'Y' ? (
+            'Partenaire'
+          ) : (
+            <span className="not-partner">Non partenaire</span>
+          ),
         cout: `${Math.floor(r.Côut * 100) / 100.0} €`,
       };
     });
@@ -94,7 +106,18 @@ class Reco extends Component {
 
     return (
       <div className="Reco">
-        <ReactTable columns={columns} data={this.state.recoTable} />
+        <ReactTable
+          columns={columns}
+          data={this.state.recoTable}
+          defaultPageSize={5}
+          noDataText="Aucun résultat"
+          ofText="sur"
+          rowsText="lignes"
+          previousText="Précédents"
+          nextText="Suivants"
+          loadingText="Chargement..."
+          loading={this.state.loading}
+        />
       </div>
     );
   }
